8fed3eda461d54a778d16d89ff43480e652191be
Hulbert Zeng <hulbert2013@gmail.com>
2021-03-04 13:08:00 -0800

    Completed Part 1

--- turnin/hzeng012_lab13_part1.c
+++ turnin/hzeng012_lab13_part1.c
@@ -7,7 +7,7 @@
  * I acknowledge all content contained herein, excluding template or example
  * code, is my own original work.
  *
- *  Demo Link: Youtube URL>
+ *  Demo Link: https://youtu.be/qH4EPJoZ_oI
  */ 
 #include <avr/io.h>
 #ifdef _SIMULATE_
